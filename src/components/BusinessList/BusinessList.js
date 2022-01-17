d7e065be3d2d5063876b75b9a3c5b49aa6ca60a2
Carneiro <paulacarneiro215@gmail.com>
2022-01-17 18:31:41 +0000

    Fix indentation

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -4,16 +4,16 @@ import Business from '../Business/Business.js';
 
 
 class BusinessList extends React.Component {
-    render() {
-        <div className="BusinessList">
-            <Business/>
-            <Business/>
-            <Business/>
-            <Business/>
-            <Business/>
-            <Business/>
-        </div>
-    }
+  render() {
+    <div className="BusinessList">
+      <Business/>
+      <Business/>
+      <Business/>
+      <Business/>
+      <Business/>
+      <Business/>
+    </div>
+  }
 }
 
 export default BusinessList;
