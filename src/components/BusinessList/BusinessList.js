697ebdbf28bdb3a9e388d5cc8e0a6da21aec9748
Carneiro <paulacarneiro215@gmail.com>
2022-01-17 16:12:49 +0000

    Create business list component

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './BusinessList.js';
+import Business from '../Business/Business.js';
+
+
+class BusinessList extends React.Component {
+    render() {
+        <div className="BusinessList">
+            <Business/>
+            <Business/>
+            <Business/>
+            <Business/>
+            <Business/>
+            <Business/>
+        </div>
+    }
+}
+
+export default BusinessList;
