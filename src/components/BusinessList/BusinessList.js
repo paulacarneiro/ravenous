25bf817782f61a22ccb9004503d4f3c8f7eec3d2
Carneiro <paulacarneiro215@gmail.com>
2022-01-18 11:14:13 +0000

    Pass down information from app

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -2,17 +2,15 @@ import React from "react";
 import './BusinessList.css';
 import Business from '../Business/Business.js';
 
-
 class BusinessList extends React.Component {
   render() {
     return (
       <div className="BusinessList">
-        <Business/>
-        <Business/>
-        <Business/>
-        <Business/>
-        <Business/>
-        <Business/>
+        {
+          this.props.businesses.map(business => {
+           return <Business business={business} />;
+          })
+        }
       </div>
     )
   }
